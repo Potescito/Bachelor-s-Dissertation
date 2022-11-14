15d0d2b5debc687892746d2066b11ab0686faf71
Potescito <118141460+Potescito@users.noreply.github.com>
2022-11-14 01:05:49 +0100

    Delete AHP_eig.m

--- AHP_eig.m
+++
@@ -1,6 +0,0 @@
-% Comparison, theoretical computed weights
-[eigenvectors, eigenvalues] = eig(cr);
-lambda_num = max(max(real(eigenvalues)));
-[~, pos_main_eig] = find(real(eigenvalues) == lambda_num);
-norm_eig_w = eigenvectors(:, pos_main_eig) / sum(eigenvectors(:, pos_main_eig));
-IRW = abs(w_cr' - norm_eig_w); % Comparison index btwn methods
