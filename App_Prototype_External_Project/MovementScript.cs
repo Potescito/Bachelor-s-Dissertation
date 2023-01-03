99b1d3cfe787670adecbc11560ab921a8b1647ef
Potescito <118141460+Potescito@users.noreply.github.com>
2023-01-03 19:52:52 +0100

    Smartphone App (External Project)
    The development of the app is by the external project in which the dissertation is framed, that is, we have not done it in its entirety.

---
+++ App_Prototype_External_Project/MovementScript.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public float velocity;
+    public TMP_Text mlText;
+    public TMP_Text maxMlText;
+    public TMP_Text numberOfHitsText;
+    public GameObject bluetoothManager;
+    public float prom = 0;
+    public float posY = 0;
+    public float posYY = 0;
+
+    NativeCodeRunner scriptBlt;
+
+    private Rigidbody2D rb;
+    private float verticalVelocity;
+    private int numberOfHits = 0;
+    public int maxMl = 0;
+
+    private bool mousePressed = false;
+
+    public GameObject windowGraphGameObject;
+    private WindowGraph windowGraph;
+    private List<int> patientExerciseValues = new List<int>();
+    private Dictionary<int, List<int>> patientCompleteExerciseDictionary = new Dictionary<int, List<int>>();
+    
+    void Start() {
+        scriptBlt = bluetoothManager.GetComponent<NativeCodeRunner>();
+        windowGraph = FindObjectOfType<WindowGraph>();
+        rb = GetComponent<Rigidbody2D>();
+        numberOfHitsText.text = numberOfHits.ToString();
+        maxMlText.text = maxMl.ToString();
+    }
+
+
+
+
+
+
+
+
+
+
+    
+    void Update() {
+        //Mouse inputs
+        if (Input.GetMouseButtonDown(0)) mousePressed = true;
+        else if (Input.GetMouseButtonUp(0)) mousePressed = false;
+        verticalVelocity = mousePressed ? 1 : -1;
+        //Touch input
+        /*switch (Input.touchCount) {
+            case 0:
+                verticalVelocity = 0;
+                break;
+            case 1:
+                verticalVelocity = 1;
+                break;
+            case 2:
+                verticalVelocity = -1;
+                break;
+            default:
+                verticalVelocity = 0;
+                break;
+        }*/
+        //Ml Update
+        //prom = (float) scriptBlt.prom * 1000;
+        //Debug.LogWarningFormat("promedio: {0}", prom.ToString());
+        //posY = GameData.GetAbsolutPositionFromMl(prom) + 0.78f;
+        int actualMl = scriptBlt.posGL;
+        mlText.text = actualMl.ToString();
+        if (actualMl > maxMl) {
+            maxMl = actualMl;
+            maxMlText.text = maxMl.ToString();
+        }
+        posY = scriptBlt.posY;
+        //Debug.LogWarningFormat("position Y: {0}", posY.ToString());
+        transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+
+        //int actualMl = GameData.GetMlPositionFromAbsolut(transform.position.y);
+        //transform.position.Set(transform.position.x, posY, transform.position.z);
+    }
+
+   /*void FixedUpdate() {
+        float fixedVelocity = (verticalVelocity > 0) ? velocity : velocity / 2;
+        rb.velocity = Vector2.up * verticalVelocity * fixedVelocity;
+    }*/
+
+    public void GetPlayerGraph(int serieCount) {
+        windowGraph.ShowGraph(patientExerciseValues, WindowGraph.EXERCISE_PATIENT_SERIE_COLOR);
+        patientCompleteExerciseDictionary.Add(serieCount, patientExerciseValues);
+        patientExerciseValues = new List<int>();
+    }
+
+    public void SavePlayerGraph(int serieCount) {
+        patientCompleteExerciseDictionary.Add(serieCount, patientExerciseValues);
+        patientExerciseValues = new List<int>();
+    }
+
+    public List<GameObject> GetSerieGraph(int serieNumber) {
+        return windowGraph.ShowGraph(patientCompleteExerciseDictionary[serieNumber], WindowGraph.EXERCISE_PATIENT_SERIE_COLOR);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            collision.gameObject.SetActive(false);
+            numberOfHits++;
+            numberOfHitsText.text = numberOfHits.ToString();
+        }
+        else if (collision.gameObject.CompareTag("Graph")) {
+            patientExerciseValues.Add(GameData.GetMlPositionFromAbsolut(transform.position.y) + (int)GameData.minCapacity);
+        }
+    }
+}
---
+++ App_Prototype_External_Project/NativeCodeRunner.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class NativeCodeRunner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AndroidJavaObject bluet;
+    AndroidJavaObject alert;
+    AndroidJavaClass unityPlayerClass;
+    AndroidJavaObject unityActivity;
+    public Double prom = 0;
+    public float posY = 0;
+    public int posGL = 0;
+
+    void Start()
+    {
+        CallNativePlugin();
+    }
+
+    //method that calls our native plugin.
+    public void CallNativePlugin()
+    {
+        // Retrieve the UnityPlayer class.
+        unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        // Retrieve the UnityPlayerActivity object ( a.k.a. the current context )
+        unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        // Retrieve the "Bridge" from our native plugin.
+        // ! Notice we define the complete package name.              
+        //alert = new AndroidJavaObject("com.millerbsv.bluetoothhc.Alert");
+        bluet = new AndroidJavaObject("com.millerbsv.bluetoothhc.Bluetooth");
+
+        //bluet = new AndroidJavaClass("com.millerbsv.bluetoothhc.Bluetooth");
+
+        // Setup the parameters we want to send to our native plugin.              
+        //object[] parameters = new object[2];
+        //parameters[0] = unityActivity;
+        //parameters[1] = "Hello World!";
+        //alert.Call("PrintString", parameters);
+
+        // Call PrintString in bridge, with our parameters.
+        string data = bluet.Call<string>("getDevices");
+        //parameters[1] = data;
+
+        posY = GameData.GetAbsolutPositionFromMl((float)prom) + 0.78f;
+
+        posGL = GameData.GetMlPositionFromAbsolut(posY);
+
+        if (data != null)
+        {
+            string[] subs = data.Split(';');
+            Debug.LogWarningFormat("Devices: {0}", data);
+            //Debug.LogWarningFormat("Device 1: {0}", subs[1]);
+
+
+            //alert.Call("PrintString", parameters);
+            object[] parameters2 = new object[2];
+            parameters2[0] = 4;
+            parameters2[1] = "00:20:12:08:B3:43";
+            bluet.Call("connectToDevice", parameters2);
+
+            InvokeRepeating("OutputTime", 2f, 0.2f);  //1s delay, repeat every 1s
+
+        }
+
+
+
+
+
+    }
+
+    void OutputTime()
+    {
+        //Debug.LogWarning("Obtiene la data");
+        string data = bluet.Call<string>("getData");
+        //string data = "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.27" + Environment.NewLine + "0.";
+        object[] parameters = new object[2];
+        //Debug.LogWarningFormat("dataBluetooth {0}", data);
+        parameters[0] = unityActivity;
+        string[] strings = Regex.Split(data, Environment.NewLine);
+        List<double> valuesList = new List<double>();
+        Double tempValue;
+        for (int i = 1; i < strings.Length-1; i++)
+        {
+            if(Double.TryParse(strings[i], out tempValue))
+            {
+                valuesList.Add(tempValue);
+            } else
+            {
+                Console.WriteLine("error parse data {0}", strings[i]);
+            }
+        }
+        Double[] valuesDouble = valuesList.ToArray();
+        if(valuesDouble.Length > 0)
+        {
+            //Debug.LogWarningFormat("valuesDouble {0}", valuesDouble.ToString());
+            prom = valuesDouble.Average() * 1000;
+            Debug.LogWarningFormat("promedio: {0}", prom.ToString());
+            posY = GameData.GetAbsolutPositionFromMl((float)prom) + 0.78f;
+            Debug.LogWarningFormat("position Y: {0}", posY.ToString());
+            posGL = GameData.GetMlPositionFromAbsolut(posY);
+            Debug.LogWarning("values : " + String.Join(",", strings));
+            //parameters[1] = prom.ToString();
+            //Debug.LogWarning(prom.ToString());
+
+            //alert.Call("PrintString", parameters);
+        }
+    }
+}
